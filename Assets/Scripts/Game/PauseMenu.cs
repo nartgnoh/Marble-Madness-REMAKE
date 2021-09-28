311a81d1113f602630ff07c250fee639da60dbd3
Hong-Hue Tran <hong.hue.tran@outlook.com>
2021-09-28 15:38:31 -0400

    Checking out menu

---
+++ Assets/Scripts/Game/PauseMenu.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetKeyDown("escape"))
+        {
+            
+        }
+    }
+}
