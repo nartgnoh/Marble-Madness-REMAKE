45d38e8ceab7d969c5040595b9736c8c20378f42
cvu232 <kuroshiruchan@gmail.com>
2021-10-02 21:48:21 -0400

    Music + Death SFX

--- Assets/Scripts/Reset.cs
+++ Assets/Scripts/Reset.cs
@@ -6,12 +6,15 @@ using UnityEngine.SceneManagement;
 public class Reset : MonoBehaviour
 {
     public float threshold = -50f;
+    public GameObject source;
+
     // Update is called once per frame
     void Update()
     {
         if (transform.position.y < threshold)
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            Instantiate(source, transform.position, Quaternion.identity); //play SFX
         }
     }
 }
